1c6805cff71829b5d6781cdf4c0abbcf4a2bde92
Yaroslav Rogovich <yaroslav.rogovich@crafton.pl>
2022-07-15 18:11:51 +0200

    [page][styles][rwd] add header (static content)

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,18 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <main>
-        Hello World!
+        <header className="header">
+          <div className="container">
+            <div className="header__row">
+              <div className="header__title">In oculis quidem rerum facilis est et aperta.</div>
+              <div className="header__image">
+                <img src="https://source.unsplash.com/random" />
+              </div>
+            </div>
+          </div>
+        </header>
       </main>
     </>
   )
